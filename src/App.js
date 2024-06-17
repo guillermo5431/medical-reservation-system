e7be9082347135bb88ed642f835856dd00fb238b
guillermo5431 <92940444+guillermo5431@users.noreply.github.com>
2024-06-17 17:23:38 -0500

    fixed links on navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Doctor from './pages/doctor';
+import Patient from './pages/patient';
+import Appointment from './pages/appointments';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+     <Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctor" element={<Doctor />} />
+          <Route path="/patient" element={<Patient />} />
+          <Route path="/appointment" element={<Appointment />} />
+        </Routes>
+     </Navbar>
     </div>
   );
 }
