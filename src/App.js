4028322e8939e7ad440a37e0f8822b8c315e0c99
Guillermo <reyesguillermo54@yahoo.com>
2024-06-21 18:13:14 -0500

    New edits

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar/index'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
      <h1>Hello World</h1>
     </div>
   );
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function index() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
